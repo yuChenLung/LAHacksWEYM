1b6da0f3149e3d363e08e8cac8b935780ce84429
yuchenlungucla <yuchenlung@g.ucla.edu>
2023-04-22 03:15:38 -0400

    can now get lat and long in Geocode

--- weym/src/Geocoding/Geocode.js
+++ weym/src/Geocoding/Geocode.js
@@ -1,77 +1,50 @@
-import React, { useState, useRef, useEffect } from 'react';
-import { loadScript } from '@react-google-maps/api';
-import loadGoogleMapsApi from 'load-google-maps-api';
+import React, { useState } from 'react';
+import { useLoadScript } from '@react-google-maps/api';
 
+const libraries = ['places'];
 
 const Geocode = () => {
-  const addressRef = useRef(null);
-  const cityRef = useRef(null);
-  const [address, setAddress] = useState('');
-  const [city, setCity] = useState('');
-  const [latitude, setLatitude] = useState(null);
-  const [longitude, setLongitude] = useState(null);
+  const [location, setLocation] = useState('');
+  const [latLng, setLatLng] = useState(null);
+  const GOOGLE_MAPS_API_KEY = 'AIzaSyCuZ4iz7AYMcmdAoDoVzjVqB2g07gvGEf0';
+  const { isLoaded, loadError } = useLoadScript({
+    googleMapsApiKey: GOOGLE_MAPS_API_KEY,
+    libraries
+  });
 
-  useEffect(() => {
-    const apiKey = 'AIzaSyCuZ4iz7AYMcmdAoDoVzjVqB2g07gvGEf0' ;
+  const handleInputChange = (event) => {
+    setLocation(event.target.value);
+  };
+
+  const handleSearchClick = () => {
     const geocoder = new window.google.maps.Geocoder();
-  
-    loadGoogleMapsApi({ key: apiKey, libraries: ['places'] }).then((googleMaps) => {
-      const addressAutocomplete = new googleMaps.places.Autocomplete(addressRef.current);
-      const cityAutocomplete = new googleMaps.places.Autocomplete(cityRef.current);
-  
-      addressAutocomplete.setFields(['address_components', 'geometry']);
-      cityAutocomplete.setFields(['address_components', 'geometry']);
-  
-      addressAutocomplete.addListener('place_changed', () => {
-        const place = addressAutocomplete.getPlace();
-  
-        if (place && place.geometry) {
-          const address = place.address_components
-            .map(component => component.long_name)
-            .join(', ');
-  
-          setAddress(address);
-  
-          geocoder.geocode({ address }, (results, status) => {
-            if (status === 'OK' && results.length > 0) {
-              const location = results[0].geometry.location;
-              setLatitude(location.lat());
-              setLongitude(location.lng());
-            }
-          });
-        }
-      });
-  
-      cityAutocomplete.addListener('place_changed', () => {
-        const place = cityAutocomplete.getPlace();
-  
-        if (place && place.geometry) {
-          const address = place.address_components
-            .map(component => component.long_name)
-            .join(', ');
-  
-          setCity(address);
-  
-          geocoder.geocode({ address }, (results, status) => {
-            if (status === 'OK' && results.length > 0) {
-              const location = results[0].geometry.location;
-              setLatitude(location.lat());
-              setLongitude(location.lng());
-            }
-          });
-        }
-      });
+    geocoder.geocode({ address: location }, (results, status) => {
+      if (status === 'OK') {
+        const { lat, lng } = results[0].geometry.location;
+        setLatLng({ lat: lat(), lng: lng() });
+        console.log(lat)
+        console.log(lng)
+      } else {
+        console.log(`Geocode was not successful for the following reason: ${status}`);
+      }
     });
-  }, []);
+  };
+
+  if (loadError) return 'Error loading maps';
+  if (!isLoaded) return 'Loading maps...';
 
   return (
-    <div>
-      <input type="text" ref={addressRef} value={address} onChange={(e) => setAddress(e.target.value)} />
-      <input type="text" ref={cityRef} value={city} onChange={(e) => setCity(e.target.value)} />
-      <p>Latitude: {latitude}</p>
-      <p>Longitude: {longitude}</p>
-    </div>
-  );  
+    <>
+      <input type="text" value={location} onChange={handleInputChange} />
+      <button onClick={handleSearchClick}>Search</button>
+      {latLng && (
+        <div>
+          <p>Latitude: {latLng.lat}</p>
+          <p>Longitude: {latLng.lng}</p>
+        </div>
+      )}
+    </>
+  );
 };
 
 export default Geocode;
