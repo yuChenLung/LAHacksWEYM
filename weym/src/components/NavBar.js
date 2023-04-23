3ced00191624d17a81d2001a9a2fa76c7c4af331
Megan Go <Meganggo11@gmail.com>
2023-04-22 18:42:50 -0700

    onboarding page

--- weym/src/components/NavBar.js
+++ weym/src/components/NavBar.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import logo from '../logo.svg';
-import { Link, useNavigate } from 'react-router-dom';
+import { Link } from 'react-router-dom';
 import './components.css';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { faUser } from '@fortawesome/free-solid-svg-icons';
